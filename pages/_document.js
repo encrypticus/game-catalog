7c0ecd28a7df74b2e30041df8bb587203f708bc3
encrypticus <encrypticus@gmail.com>
2021-04-01 18:27:12 +0600

    Добавил кастомный _document.

---
+++ pages/_document.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import { ServerStyleSheets } from '@material-ui/core/styles';
+import Document, {
+  Html, Head, Main, NextScript,
+} from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <style jsx global>
+            {`
+            #__next {
+              height: 100%;
+            }
+          `}
+          </style>
+        </body>
+      </Html>
+    );
+  }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () => originalRenderPage({
+    enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+  });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+  };
+};
